8c9bb269c3fafb9305aac171a9249a1f1d98d454
Leo <54489302+EspinozaLeo@users.noreply.github.com>
2026-05-21 16:54:50 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        // this is O(n^2) because every string concat will run n times. use StringBuilder
+        // String encodedWords = "";
+        // for(String s : strs){
+        //     encodedWords = encodedWords + s.length() + "#" + s;
+        // }
+        // return encodedWords;
+        
+        //this is O(n)
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs){
+            sb.append(s.length()).append('#').append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> decodedStrings = new ArrayList<>();
+        int i = 0;
+        while(i < str.length()){
+            int delimiter = str.indexOf("#", i);
+            String prefix = str.substring(i, delimiter);
+            int wordSize = Integer.parseInt(prefix);
+            String decodedWord = str.substring(delimiter + 1, delimiter + 1 + wordSize);
+            decodedStrings.add(decodedWord);
+            i = delimiter + 1 + wordSize;
+        }
+
+        return decodedStrings;
+    }
+}
