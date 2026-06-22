bb2ca0e9e461a6be1fc6d37cc3c991dedb1d7e71
Leo <54489302+EspinozaLeo@users.noreply.github.com>
2026-06-22 17:13:15 -0400

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // int[] outputArray = new int[nums.length - k + 1];
+        // int left = 0, right = k - 1, i = 0;
+        // while(right < nums.length){
+        //     outputArray[i] = Arrays.stream(Arrays.copyOfRange(nums, left, right + 1)).max().getAsInt();
+        //     i++;
+        //     left++;
+        //     right++;
+        // }
+        // return outputArray;
+
+        int numsLength = nums.length;
+        int[] outputArray = new int[numsLength - k + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        int left = 0, right = 0;
+        while(right < numsLength){
+            while(q.size() > 0 && nums[q.getLast()] < nums[right]){
+                q.removeLast();
+            }
+            q.addLast(right);
+
+            //expire the maximum in the queue once the window slides past it
+            if(left > q.getFirst()){
+                q.removeFirst();
+            }
+
+            //check if window is valid (size of k)
+            if((right + 1) >= k){
+                outputArray[left] = nums[q.getFirst()];
+                left++;
+            }
+            right++;
+        }
+
+        return outputArray;
+    }
+}
