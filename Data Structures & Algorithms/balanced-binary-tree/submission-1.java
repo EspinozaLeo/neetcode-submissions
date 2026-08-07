a2c63e25c9046d5f3295587f0ffbdbc29b024f5c
Leo <54489302+EspinozaLeo@users.noreply.github.com>
2026-08-07 11:40:44 -0400

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // boolean isItBalanced[] = new boolean[1];
+        // isItBalanced[0] = true;
+        // dfs(root, isItBalanced);
+        // return isItBalanced[0];
+
+        return dfs(root) != -1;
+    }
+
+    // private int dfs(TreeNode currRoot, boolean[] arr){
+        // if(currRoot == null) return 0;
+
+        // int leftTree = dfs(currRoot.left, arr);
+        // int rightTree = dfs(currRoot.right, arr);
+        // if(Math.abs(leftTree - rightTree) > 1) arr[0] = false;
+        // return 1 + Math.max(leftTree, rightTree);
+    // }
+    private int dfs(TreeNode currRoot){
+        if(currRoot == null) return 0;
+
+        int leftTree = dfs(currRoot.left);
+        if(leftTree == -1) return -1;
+        int rightTree = dfs(currRoot.right);
+        if(rightTree == -1) return -1;
+        if(Math.abs(leftTree - rightTree) > 1) return -1;
+        return 1 + Math.max(leftTree, rightTree);
+    }
+}
